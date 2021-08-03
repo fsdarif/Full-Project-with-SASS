a45c31af3ac0467cf01ffc616e7f01bb495fbe39
Ahnaf Arif <2080arif@gmail.com>
2021-08-03 15:02:41 +0600

    added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.project-popup').magnificPopup({
+        type:'image',
+        gallery:{
+            enabled:true
+          }
+});
+
+$('.video').magnificPopup({
+  type:'iframe'
+});
+
+
+    $('.counter').counterUp();
+    $('.testimonials').slick({
+      prevArrow:'<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>',
+      dots:true,
+    });
+  });
